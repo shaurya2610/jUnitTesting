7553c4791072e0f9d7d0fa9618d24011bb0bef7a
tspsc <tspsc@LAPTOP-BLN2TBNR>
2021-04-16 16:00:09 +0530

    jUnitTesting

---
+++ jUnitTesting/src/jUnitTestingPackage/jUnitFunction.java
@@ -0,0 +1,14 @@
+package jUnitTestingPackage;
+
+public class jUnitFunction {
+	public int addNumbers(int a,int b)
+	{
+		return a+b;
+	}
+	public String addStrings(String x,String y)
+	{
+		return x+y;
+	}
+
+
+}
